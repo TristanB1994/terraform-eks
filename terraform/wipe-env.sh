5fa47c877bdebb084cf8f02c204b958fbd17a36a
TristanB1994 <t.blandford.dev@gmail.com>
2023-05-08 23:06:40 -0400

    add terraform files

---
+++ terraform/wipe-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+terraform destroy -target=module.cluster.module.eks_blueprints_kubernetes_addons --auto-approve
+terraform destroy -target=module.cluster.module.descheduler --auto-approve
+terraform destroy -target=module.cluster.module.eks_blueprints --auto-approve
+terraform destroy --auto-approve
